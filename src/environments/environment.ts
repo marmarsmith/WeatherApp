cc315c4c0599cd7c4ae22c16f30e06eb02b450c7
Marcel Smith <91510178+marmarsmith@users.noreply.github.com>
2023-01-15 14:19:14 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'b46c3d1991mshcb97831f79a20c1p15a4c8jsnc8bcdca7046f',
+
+
+};
